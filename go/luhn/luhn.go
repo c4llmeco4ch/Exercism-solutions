cfcaba8b69d714fd2db1867dbccde5c3271a283f
c4llmeco4ch <cbb692@gmail.com>
2019-12-30 10:27:33 -0800

    Combined Valid and initslice, cleaned up valid

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -2,39 +2,27 @@ package luhn
 
 //Valid determine whether a provided string is a valid
 func Valid(s string) bool {
-	arr, sum, ok := convertToIntSlice(s)
-	if len(arr) <= 1 || !ok {
-		return false
-	}
-	return sum%10 == 0
-}
-
-func convertToIntSlice(s string) ([]int, int, bool) {
-	arr := make([]int, len(s))
-	pos := len(s) - 1
-	everyOther := 1
 	numSpaces := 0
 	sum := 0
-	for pos >= 0 {
+	for pos := len(s) - 1; pos >= 0; pos-- {
 		if s[pos] == ' ' {
-			pos--
 			numSpaces++
 			continue
 		}
-		if int(s[pos]) < '0' || int(s[pos]) > '9' {
-			return arr, -1, false
+		if s[pos] < '0' || s[pos] > '9' {
+			return false
 		}
 		val := int(s[pos] - '0')
-		if everyOther%2 == 0 {
+		if pos%2 == (len(s)-numSpaces)%2 {
 			val *= 2
 			if val > 9 {
 				val -= 9
 			}
 		}
-		arr[pos] = val
 		sum += val
-		everyOther = (everyOther + 1) % 2
-		pos--
 	}
-	return arr, sum, len(s)-numSpaces > 1
+	if len(s)-numSpaces <= 1 {
+		return false
+	}
+	return sum%10 == 0
 }
