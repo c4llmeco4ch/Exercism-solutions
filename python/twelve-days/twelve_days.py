07e8e079fed9db6cad4fb86bf2b833050b618b83
c4llmeco4ch <cbb692@gmail.com>
2019-12-29 19:54:00 -0800

    Dropped recite to one line, reduced the number of join calls

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,16 +1,20 @@
-days = [['first', 'a Partridge in a Pear Tree'],
-        ['second', 'two Turtle Doves'],
-        ['third', 'three French Hens'],
-        ['fourth', 'four Calling Birds'],
-        ['fifth', 'five Gold Rings'],
-        ['sixth', 'six Geese-a-Laying'],
-        ['seventh', 'seven Swans-a-Swimming'],
-        ['eighth', 'eight Maids-a-Milking'],
-        ['ninth', 'nine Ladies Dancing'],
-        ['tenth', 'ten Lords-a-Leaping'],
-        ['eleventh', 'eleven Pipers Piping'],
-        ['twelfth', 'twelve Drummers Drumming'],
+gifts = ['a Partridge in a Pear Tree',
+         'two Turtle Doves',
+         'three French Hens',
+         'four Calling Birds',
+         'five Gold Rings',
+         'six Geese-a-Laying' ,
+         'seven Swans-a-Swimming',
+         'eight Maids-a-Milking',
+         'nine Ladies Dancing',
+         'ten Lords-a-Leaping',
+         'eleven Pipers Piping',
+         'twelve Drummers Drumming',
         ]
+
+days = ['first', 'second', 'third', 'fourth', 
+        'fifth', 'sixth', 'seventh', 'eighth',
+        'ninth', 'tenth', 'eleventh', 'twelfth']
 
 
 def recite(start_verse, end_verse):
@@ -19,10 +23,8 @@ def recite(start_verse, end_verse):
 
 def calc_day(day):
     text = ('On the %s day of Christmas ' +
-            'my true love gave to me: ') % days[day][0]
-    for i in range(day, -1, -1):
-        text = ''.join([text,
-                        (', ' if i != day else ''),
-                        ('and ' if day != 0 and i == 0 else ''),
-                        days[i][1]])
-    return ''.join([text, '.'])
+            'my true love gave to me: ') % days[day]
+    all_gifts = ', '.join([gifts[i] for i in range(day, 0, -1)])
+    return ''.join([text, all_gifts, 
+                    ', and ' if day > 0 else '', 
+                    gifts[0], '.'])
