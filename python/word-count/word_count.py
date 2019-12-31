9186a536b7194f388253e1fa4243566bf52c4e9c
c4llmeco4ch <cbb692@gmail.com>
2019-12-30 16:28:34 -0800

    Submitted first solution attempt

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,16 @@
+from string import punctuation
 def count_words(sentence):
-    pass
+    dict = {}
+    punc = punctuation.replace("'",'')
+    for val in punc:
+        if val in sentence:
+            sentence = sentence.replace(val, ' ')
+    for word in sentence.split():
+        if (w := word.lower())[0] == "'":
+            w = w[1:]
+        if w[-1] == "'":
+            w = w[:-1]
+        if w not in dict:
+            dict[w] = 0
+        dict[w] += 1
+    return dict
