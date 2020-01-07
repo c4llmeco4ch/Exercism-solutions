55f7e9c6e43c3b74f14c305a15af914a4a7df73a
c4llmeco4ch <cbb692@gmail.com>
2020-01-07 14:54:35 -0800

    Removed the need to calculate len - numSpaces

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -2,24 +2,24 @@ package luhn
 
 //Valid determine whether a provided string is a valid
 func Valid(s string) bool {
-	numSpaces := 0
+	realPos := 0
 	sum := 0
 	for pos := len(s) - 1; pos >= 0; pos-- {
 		if s[pos] == ' ' {
-			numSpaces++
 			continue
 		}
 		if s[pos] < '0' || s[pos] > '9' {
 			return false
 		}
 		val := int(s[pos] - '0')
-		if pos%2 == (len(s)-numSpaces)%2 {
+		if realPos%2 == 1 {
 			val *= 2
 			if val > 9 {
 				val -= 9
 			}
 		}
+		realPos++
 		sum += val
 	}
-	return sum%10 == 0 && len(s)-numSpaces > 1
+	return sum%10 == 0 && realPos > 1
 }
