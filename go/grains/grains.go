719ea524c2f6466b448f387ef4ed196f333a521e
c4llmeco4ch <cbb692@gmail.com>
2020-02-16 17:03:26 -0800

    removed call to Square

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -6,25 +6,19 @@ import (
 
 const start uint64 = 1
 
-//Square return the number of grains at space 'input'
-func Square(input int) (uint64, error) {
+//Total return the total number of grains for all spaces on the board
+func Total(input int, wantSingle bool) (uint64, error) {
 	if input <= 0 || input > 64 {
 		return 0, errors.New("Invalid number entered")
 	}
-	return start << (input - 1), nil
-}
-
-//Total return the total number of grains for all spaces on the board
-func Total() uint64 {
-	top, pos := 64, 1
+	top, pos := input, 1
 	sum := uint64(0)
 	for top >= pos {
-		val, err := Square(pos)
 		if err != nil {
-			return 0 //Dealing with invald pos reached
+			return 0, errors.New("Invalid square #%d reached", pos) //Dealing with invald pos reached
 		}
-		sum += val
+		sum += (start << (pos - 1))
 		pos++
 	}
-	return sum
+	return sum, nil
 }
