e763396f543587f6b159101e070fa124e3998580
c4llmeco4ch <cbb692@gmail.com>
2020-03-01 18:24:25 -0800

    implemented re.split() over str.split() to complete the one-liner

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,15 +1,7 @@
-import string #, re
+import string, re
 def abbreviate(words):
-    ans = ''
-    for word in words.split():
-        cut = word.strip(string.punctuation)
-        if len(cut) == 0:
-            continue
-        if '-' in cut:
-            for s in cut.split('-'):
-                ans += s[0].upper()
-        else:
-            ans += cut[0].upper()
-    return ans
-
-    # So close: return ''.join(letter.upper() for word in words.split() if re.match('[a-zA-Z]', letter := word[0]))
+    """
+    Given the proliferation of 1337speak, 
+    I'm allowing for numbers and letters to both be included in acronyms
+    """
+    return ''.join(letter.upper() for word in re.split(r'[-_ ]',words) if len(word) > 0 and re.match('[a-z0-9A-Z]', letter := word[0]))
