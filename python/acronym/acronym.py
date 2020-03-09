6e8688bc1440e32dbe95201e94f0292053ce4bfb
c4llmeco4ch <cbb692@gmail.com>
2020-03-08 19:20:23 -0700

    Altered line structure for solution

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -4,4 +4,5 @@ def abbreviate(words):
     Given the proliferation of 1337speak, 
     I'm allowing for numbers and letters to both be included in acronyms
     """
-    return ''.join(letter.upper() for word in re.split(r'[-_ ]',words) if len(word) > 0 and re.match('[a-z0-9A-Z]', letter := word[0]))
+    return ''.join(letter.upper() for word in re.split(r'[-_ ]',words) 
+           if len(word) > 0 and re.match('[a-z0-9A-Z]', letter := word[0]))
