72b6fa20e77277520d23c2df0b1ad6e06223db9a
c4llmeco4ch <cbb692@gmail.com>
2020-03-28 16:20:25 -0700

    Removed walrus usage for simpler readability

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,6 +1,6 @@
 import itertools
 
-plant_alts = {'R' : 'Radishes', 'G' : 'Grass', 'C' : 'Clover', 'V' : 'Violets'}
+PLANT_ALTS = {'R' : 'Radishes', 'G' : 'Grass', 'C' : 'Clover', 'V' : 'Violets'}
 class Garden:
     """Given a selection of student names and a complementary diagram, create a classroom garden"""
     def __init__(self, diagram, students = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]):
@@ -9,9 +9,9 @@ class Garden:
     
     def plants(self, student):
         """Given a student, return a list of plants they are responsible for"""
-        group = self.students.index(student)
-        vals = list(itertools.chain(self.diagram[0][(start := (group*2)): start + 2], self.diagram[1][start:start + 2]))
+        group = self.students.index(student) * 2
+        vals = list(itertools.chain(self.diagram[0][group:group + 2], self.diagram[1][group:group + 2]))
         for pos, plant in enumerate(vals):
-            vals[pos] = plant_alts[plant]
+            vals[pos] = PLANT_ALTS[plant]
         return vals
 
