c71d4a9d3a0f0c7972d1b7ce8419d69ff6671d30
c4llmeco4ch <cbb692@gmail.com>
2020-03-31 02:11:00 -0700

    Altered functions so clock only holds minutes

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,37 +4,37 @@ import "fmt"
 
 //Clock a clock struct providing the hours and minutes of a certain time
 type Clock struct {
-	Hours   int
 	Minutes int
 }
 
 //New Create a new clock provided an hour and minute value
 func New(h int, m int) Clock {
-	hours := h
-	if m >= 60 {
-		hours += m / 60
-	}
 	for m < 0 {
+		h--
 		m += 60
-		hours--
 	}
-	for hours < 0 {
-		hours += 24
+	for h < 0 {
+		h += 24
 	}
-	return Clock{hours % 24, m % 60}
+	return Clock{(m + h*60) % 1440} // 1440 = minutes in a day
 }
 
 //String the stringer function for a referenced clock
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.Hours, c.Minutes)
+	h := 0
+	m := c.Minutes
+	if m >= 60 {
+		h += m / 60
+	}
+	return fmt.Sprintf("%02d:%02d", h%24, m%60)
 }
 
 //Add provided some minutes 'm', add that much time to the referenced clock
-func (c *Clock) Add(m int) Clock {
-	return New(c.Hours, c.Minutes+m)
+func (c Clock) Add(m int) Clock {
+	return New(0, c.Minutes+m)
 }
 
 //Subtract provided some minutes 'm', subtract that much time to the referenced clock
-func (c *Clock) Subtract(m int) Clock {
-	return New(c.Hours, c.Minutes-m)
+func (c Clock) Subtract(m int) Clock {
+	return New(0, c.Minutes-m)
 }
