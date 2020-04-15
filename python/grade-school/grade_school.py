b08ca54e79add53f5378bba4418117b4dea57932
c4llmeco4ch <cbb692@gmail.com>
2020-04-15 11:07:13 -0700

    Changed all_grades to be a defaultdict

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,22 +1,23 @@
+from collections import defaultdict
+import typing
+from typing import List
 class School:
-    def __init__(self):
-        self.all_grades = {}
+    def __init__(self) -> None: 
+        self.all_grades = defaultdict(list)
 
-    def add_student(self, name, grade):
+    def add_student(self, name: str, grade: int) -> None:
         """
         given a student and grade,
         add that student to the grade's roster
         """
-        if grade not in self.all_grades:
-            self.all_grades[grade] = []
         self.all_grades[grade].append(name)
 
-    def roster(self):
+    def roster(self) -> List[List[str]]:
         """return the roster sorted by grade then by name"""
         return [student for g in sorted(self.all_grades.keys())
                 for student in self.grade(g)]
 
-    def grade(self, grade_number):
+    def grade(self, grade_number: int) -> List[str]:
         """return a sorted list of the students in a grade"""
         return sorted(self.all_grades[grade_number])\
             if grade_number in self.all_grades else []
