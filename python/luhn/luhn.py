61ff4a6ff6f7daec8165b0fe41f2606f7e9d8dcb
c4llmeco4ch <cbb692@gmail.com>
2020-04-15 22:21:19 -0700

    Removed try-except block

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,5 +1,6 @@
 import typing
 import itertools
+from string import digits
 
 class Luhn:
     def __init__(self, card_num: str) -> None:
@@ -9,24 +10,21 @@ class Luhn:
     '''for fun
     def valid(self) -> bool:
         """Confirm whether or not the provided card number is a valid number"""
-        try:
-            return len(self.num) != 1 and (sum(itertools.chain([int(val) for val in self.num[::-2]],
-                                           [(dub := int(val) * 2) - (0 if dub <= 9 else 9) for val in self.num[-2::-2]])) % 10) == 0
-        except ValueError:
-            return False
-            
+        return (not any(i for i in self.num if i not in digits)) and\
+                len(self.num) != 1 and\
+                (sum(itertools.chain([int(val) for val in self.num[::-2]],
+                 [(dub := int(val) * 2) - (0 if dub <= 9 else 9) for val in self.num[-2::-2]])) % 10) == 0
     '''
 
     # for readability
     def valid(self) -> bool:
         """Confirm whether or not the provided card number is a valid number"""
-        try:
-            same_vals = [int(i) for i in self.num[::-2]]
-            doubled = [int(val) * 2 for val in self.num[-2::-2]]
-            for pos, num in enumerate(doubled):
-                if num > 9:
-                    doubled[pos] = num - 9
-            same_vals.extend(doubled)
-            return len(same_vals) != 1 and (sum(same_vals) % 10) == 0
-        except ValueError:
+        if any(i for i in self.num if i not in digits):
             return False
+        same_vals = [int(i) for i in self.num[::-2]]
+        doubled = [int(val) * 2 for val in self.num[-2::-2]]
+        for pos, num in enumerate(doubled):
+            if num > 9:
+                doubled[pos] = num - 9
+        same_vals.extend(doubled)
+        return len(same_vals) != 1 and (sum(same_vals) % 10) == 0
