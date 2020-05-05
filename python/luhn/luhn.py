a7287653426a868b69bdac4bcfd6636352631334
c4llmeco4ch <cbb692@gmail.com>
2020-05-04 17:43:21 -0700

    cleaned up one liner

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -10,8 +10,7 @@ class Luhn:
     '''for fun
     def valid(self) -> bool:
         """Confirm whether or not the provided card number is a valid number"""
-        return (not any(i for i in self.num if i not in digits)) and\
-                len(self.num) != 1 and\
+        return (self.num.isdigit()) and len(self.num) != 1 and\
                 (sum(itertools.chain([int(val) for val in self.num[::-2]],
                  [(dub := int(val) * 2) - (0 if dub <= 9 else 9) for val in self.num[-2::-2]])) % 10) == 0
     '''
@@ -19,10 +18,10 @@ class Luhn:
     # for readability
     def valid(self) -> bool:
         """Confirm whether or not the provided card number is a valid number"""
-        if not self.num.isdigit():
+        if not self.num.isdigit() or len(self.num) != 1:
             return False
         same_vals = [int(i) for i in self.num[::-2]]
         doubled = [int(val) * 2 for val in self.num[-2::-2]]
         combined = [num - (0 if num <= 9 else 9) for num in doubled]
         combined.extend(same_vals)
-        return len(combined) != 1 and (sum(combined) % 10) == 0
+        return (sum(combined) % 10) == 0
