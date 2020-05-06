703b731cd44bf8a340973fb995fc82c09e2a52c6
c4llmeco4ch <cbb692@gmail.com>
2020-05-05 18:15:39 -0700

    Initial submission for clock

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,15 +1,37 @@
 class Clock:
+
+    HOURS_IN_DAY = 24
+
     def __init__(self, hour, minute):
-        pass
+        self.hour = hour
+        self.minute = minute
+        self.reduce_minutes()
+
 
     def __repr__(self):
-        pass
+        ''' Is there a way to enforce 0s with an f-string?
+        return f'{self.hour}:{self.minute}'
+        '''
+        return '%02d:%02d' % (self.hour, self.minute)
 
     def __eq__(self, other):
-        pass
+        return self.minute == other.minute and self.hour == other.hour
 
     def __add__(self, minutes):
-        pass
+        self.minute += minutes
+        self.reduce_minutes()
+        return self
 
     def __sub__(self, minutes):
-        pass
+        self.minute -= minutes
+        self.reduce_minutes()
+        return self
+
+    def reduce_minutes(self):
+        while self.minute >= 60:
+            self.minute -= 60
+            self.hour += 1
+        while self.minute < 0:
+            self.minute += 60
+            self.hour -= 1
+        self.hour %= self.HOURS_IN_DAY
